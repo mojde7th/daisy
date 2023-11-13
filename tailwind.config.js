f2576a1e9299fe22583f8856c3f909922be6fc9d
Mojde7th <mojde7th@gmail.com>
2023-11-13 13:27:54 +0330

    TailwindHover

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        widthmojde: "400px",
+      },
+      spacing: {
+        leftmojde: "50%",
+      },
+      inset: {
+        insetmojde:"calc(50% - 400px / 2)"
+      },
+    },
   },
   plugins: [],
 };
